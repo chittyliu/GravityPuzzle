3fb9772a7c9c6e99eb5aabd21ce2cd5b9450fe56
Chitty Liu <d2921118@gmail.com>
2018-09-19 17:42:16 +0800

    code sorted

--- techinterview.rb
+++ techinterview.rb
@@ -1,33 +1,29 @@
 class GravityPuzzleEm
-  def columns(answer)
+  def solve(answer)
     # define an empty array
     columns = []
     # iterate the array
     answer.each do |string|
       # iterate the string
       # => ["#", "."], [".", "#"], ["#", "#"]
-      string.split("").each_with_index do |char, index|
+      string.split('').each_with_index do |char, index|
         # if the columns/array is empty then push the char indside the columns
         if columns[index]
           columns[index] << char
-        # else make it in an array and push it to the masterarray
+          # else make it in an array and push it to the masterarray
         else
           columns[index] = [char]
         end
       end
     end
     # will be an array of arraies of strings
-    # => [["#", "."], [".", "#"], ["#", "#"]]
-    return columns
-  end
-
-  # run the previous method and store it in a var
-  columns = columns(answer)
-
-  def swap(columns)
-    # awap the order in the list
+    # => [["#", ".", "#"], ["#", "#", "."]]
     columns.map! do |col|
       col.sort.reverse!
+      # => [[".", "#", "#"], [".", ".", "#"], [".", ".", "#"]]
+      col.join('')
+      # => [".##", "..#", "..#"]
     end
   end
+  p solve(["##.", "..#", "#.."])
 end
